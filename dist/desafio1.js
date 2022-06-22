12e72d8c42a60a31b722f387e9901734697c130a
Leonardo Alves <littocraft@gmail.com>
2022-06-21 23:03:32 -0300

    Desafio 1

---
+++ dist/desafio1.js
@@ -0,0 +1,10 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros?
+/* let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+// Solução
+let employee = {
+    code: 10,
+    name: "John"
+};
